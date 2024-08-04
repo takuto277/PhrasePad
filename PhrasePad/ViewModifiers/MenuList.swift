3d8835894df587d61e7a4eb6fc5f2d5cb910b4db
takuto277 <takuto.72.soccer@gmail.com>
2024-08-05 01:07:30 +0900

    Refactoring

--- PhrasePad/ViewModifiers/MenuList.swift
+++ PhrasePad/ViewModifiers/MenuList.swift
@@ -7,12 +7,67 @@
 
 import SwiftUI
 
-struct MenuList: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+// メニューリスト設定
+struct MenuList: ViewModifier {
+    @Binding var showMenu: Bool
+    @Binding var path: [Path]
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+                VStack {
+                    if showMenu {
+                        VStack(alignment: .leading, spacing: 10) {
+                            Button {
+                                showMenu.toggle()
+                            } label: {
+                                HStack {
+                                    Image(systemName: "arrow.up.arrow.down")
+                                        .frame(width: 30)
+                                    Text("整理")
+                                }
+                            }
+                            .padding(.vertical, 5)
+                            .frame(width: 70)
+                            
+                            Button {
+                                showMenu.toggle()
+                            } label: {
+                                HStack {
+                                    Image(systemName: "trash")
+                                        .frame(width: 30)
+                                    Text("削除")
+                                }
+                                .foregroundStyle(.red)
+                            }
+                            .padding(.vertical, 5)
+                            .frame(width: 70)
+                            
+                            Button {
+                                path.append(.gear)
+                                showMenu.toggle()
+                            } label: {
+                                HStack {
+                                    Image(systemName: "gear")
+                                        .frame(width: 30)
+                                    Text("設定")
+                                }
+                            }
+                            .padding(.vertical, 5)
+                            .frame(width: 70)
+                            
+                        }
+                        .padding()
+                        .background(.gray)
+                        .foregroundStyle(.black)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        .padding(.top, 40)
+                        .transition(.opacity)
+                    }
+                }
+                    .animation(.easeInOut, value: showMenu),
+                alignment: .topLeading
+            )
     }
-}
-
-#Preview {
-    MenuList()
 }
--- PhrasePad/ViewModifiers/Toolbar.swift
+++ PhrasePad/ViewModifiers/Toolbar.swift
@@ -5,4 +5,56 @@
 //  Created by 小野拓人 on 2024/08/05.
 //
 
-import Foundation
+import SwiftUI
+
+// 上部ツールバー設定
+struct Toolbar: ViewModifier {
+    @Binding var showMenu: Bool
+    @Binding var path: [Path]
+    
+    func body(content: Content) -> some View {
+        content
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    listIcon()
+                }
+                
+                ToolbarItem(placement: .principal) {
+                    imageTitle()
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    plusIcon()
+                }
+            }
+    }
+    
+    private func listIcon() -> some View {
+        Button {
+            withAnimation {
+                showMenu.toggle()
+            }
+        } label: {
+            Image(systemName: "list.bullet")
+                .fontWeight(.bold)
+        }
+    }
+    
+    private func imageTitle() -> some View {
+        Image("nou")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 30)
+    }
+    
+    private func plusIcon() -> some View {
+        ZStack(alignment: .topTrailing) {
+            Button {
+                path.append(.plus)
+            } label: {
+                Image(systemName: "plus.circle")
+                    .fontWeight(.bold)
+            }
+        }
+    }
+}
